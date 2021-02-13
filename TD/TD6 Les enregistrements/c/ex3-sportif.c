589b8eee14c1b6ed86634cc4798c5a5903cd6a96
tahe-ba <taha.ben.a@outlook.com>
2021-02-13 10:44:42 +0100

    formatter

--- TD/TD6 Les enregistrements/c/ex3-sportif.c
+++ TD/TD6 Les enregistrements/c/ex3-sportif.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 #include<string.h>
+
 #define N 3
 #define M 3
+
 typedef struct {
     int num;
     char nom[30];
@@ -17,74 +19,74 @@ void remplir_sportif(sportif * f) {
     scanf("%[^\n]", f -> nom);
     printf("\tNationalité: ");
     scanf("%s", full);
-    sprintf(f->nat, "%c%c%c",full[0],full[1],full[2] );
-    if(strlen(full)>3) printf("\t\tSeulement les trois premiers caracteres sont enregistrés\n");
+    sprintf(f -> nat, "%c%c%c", full[0], full[1], full[2]);
+    if (strlen(full) > 3) printf("\t\tSeulement les trois premiers caracteres sont enregistrés\n");
 }
 
-void remplir_n_sportif(sportif* f,int n ){
+void remplir_n_sportif(sportif * f, int n) {
     int i;
-    for (i=0;i<n;i++){
-        printf("veillez entrez les information du sportif n° %d\n",i+1);
-        remplir_sportif(f+i);
+    for (i = 0; i < n; i++) {
+        printf("veillez entrez les information du sportif n° %d\n", i + 1);
+        remplir_sportif(f + i);
     }
 }
 
 void afficher_sportif(sportif f) {
-    printf("\tnom: %s\n", f.nom);
     printf("\tnum: %d\n", f.num);
+    printf("\tnom: %s\n", f.nom);
     printf("\tnat: %s\n", f.nat);
 }
 
-void afficher_n_sportif(sportif f[],int n) {
+void afficher_n_sportif(sportif f[], int n) {
     int i;
-    for (i=0;i<n;i++){
-        printf("information du sportif n° %d\n",i+1);
+    for (i = 0; i < n; i++) {
+        printf("information du sportif n° %d\n", i + 1);
         afficher_sportif(f[i]);
     }
 }
 
 //from f to p
-void copie_sportif(sportif f,sportif* p) {
-    p->num=f.num;
-    strcpy(p->nom,f.nom);
-    strcpy(p->nat,f.nat);
+void copie_sportif(sportif f, sportif * p) {
+    p -> num = f.num;
+    strcpy(p -> nom, f.nom);
+    strcpy(p -> nat, f.nat);
 }
 
-int compare_sportif(sportif f, sportif p){
-    int x=strcmp(f.nom,p.nom),y=strcmp(f.nat,p.nat);
-    if ((f.num==p.num)&&(x==0)&&(y==0)){
+int compare_sportif(sportif f, sportif p) {
+    int x = strcmp(f.nom, p.nom), y = strcmp(f.nat, p.nat);
+    if ((f.num == p.num) && (x == 0) && (y == 0)) {
         return 1;
     } else return 0;
 }
 
-void recherche_commun(sportif f[],sportif p[],sportif * com,int n,int m,int* r){
-    int i,z,k,j=0;
-    for (i=0;i<n;i++){
-        for(z=0;z<m;z++){
-            if ((compare_sportif(f[i],p[z]))==1){
-                copie_sportif(f[i],com+j);
+void recherche_commun(sportif f[], sportif p[], sportif * com, int n, int m, int * r) {
+    int i, z, k, j = 0;
+    for (i = 0; i < n; i++) {
+        for (z = 0; z < m; z++) {
+            if ((compare_sportif(f[i], p[z])) == 1) {
+                copie_sportif(f[i], com + j);
                 j++;
             }
         }
     }
-    *r=j;
+    * r = j;
 }
 
-void main(){
-sportif d1[N];
-sportif d2[M];
-sportif commun[N+M] ;
-int a;
-remplir_n_sportif(d1,N);
-remplir_n_sportif(d2,M);
-//afficher_n_sportif(d1,N);
-recherche_commun(d1,d2,&commun,N,M,&a);
-if(a!=0){
-afficher_n_sportif(commun,a);
-}else printf("aucun sportif");
-/*
-remplir_sportif(&d1[0]);
-copie_sportif(d1[0],&d1[1]);
-afficher_sportif(d1[0]);
-*/
+void main() {
+    sportif d1[N];
+    sportif d2[M];
+    sportif commun[N + M];
+    int a;
+    /*int n,m;
+    printf("Combien de sportifs sont inscrit dans la 1ere discipline:");
+    scanf("%d",&n);
+    printf("Combien de sportifs sont inscrit dans la 1ere discipline:");
+    scanf("%d",&m);*/
+    remplir_n_sportif(d1, N);
+    remplir_n_sportif(d2, M);
+    recherche_commun(d1, d2, & commun, N, M, & a);
+    if (a > 0) {
+        printf("Les sportif inscrit dans les deux disciplines sont:\n");
+        afficher_n_sportif(commun, a);
+    } else printf("aucun sportif inscrit dans les deux\n");
 }
